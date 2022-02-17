fb310d0e5ba6c686d66bdfceb74b5c0d3c909576
Gabriel Trudes Melo <esquecimeunome00@hotmail.com>
2022-02-17 18:01:56 -0300

    Modificações no diretório service

--- src/main/java/br/com/alura/spring/data/service/RelatorioFuncionarioDinamico.java
+++ src/main/java/br/com/alura/spring/data/service/RelatorioFuncionarioDinamico.java
@@ -1,7 +1,10 @@
 package br.com.alura.spring.data.service;
 
 import org.springframework.data.jpa.domain.Specification;
+import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.Scanner;
 
@@ -9,9 +12,12 @@ import br.com.alura.spring.data.orm.Funcionario;
 import br.com.alura.spring.data.repository.FuncionarioRepository;
 import br.com.alura.spring.data.specification.SpecificationFuncionario;
 
+@Service
 public class RelatorioFuncionarioDinamico {
 
     private final FuncionarioRepository funcionarioRepository;
+
+    private final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
 
     public RelatorioFuncionarioDinamico(FuncionarioRepository funcionarioRepository) {
         this.funcionarioRepository = funcionarioRepository;
@@ -21,8 +27,43 @@ public class RelatorioFuncionarioDinamico {
         System.out.println("Digite um nome");
         String nome = scanner.next();
 
+        if (nome.equalsIgnoreCase("NULL")) {
+            nome = null;
+        }
+
+        System.out.println("Digite o cpf");
+        String cpf = scanner.next();
+
+        if (cpf.equalsIgnoreCase("NULL")) {
+            cpf = null;
+        }
+
+        System.out.println("Digite o salário");
+        Double salario = scanner.nextDouble();
+
+        if (salario == 0) {
+            salario = null;
+        }
+
+        System.out.println("Digite a data de contratação");
+        String data = scanner.next();
+
+        LocalDate dataContratacao;
+
+        if (data.equals("NULL")) {
+            dataContratacao = null;
+        } else {
+            dataContratacao = LocalDate.parse(data, formatter);
+        }
+
         List<Funcionario> funcionarios = funcionarioRepository
-            .findAll(Specification.where(SpecificationFuncionario.nome(nome)));
+            .findAll(Specification.where
+                (SpecificationFuncionario.nome(nome))
+                 .or(SpecificationFuncionario.cpf(cpf))
+                 .or(SpecificationFuncionario.salario(salario))
+                 .or(SpecificationFuncionario.dataContratacao(dataContratacao))
+            );
+        funcionarios.forEach(System.out::println);
     }
 
 }
--- src/main/java/br/com/alura/spring/data/specification/SpecificationFuncionario.java
+++ src/main/java/br/com/alura/spring/data/specification/SpecificationFuncionario.java
@@ -2,6 +2,8 @@ package br.com.alura.spring.data.specification;
 
 import org.springframework.data.jpa.domain.Specification;
 
+import java.time.LocalDate;
+
 import br.com.alura.spring.data.orm.Funcionario;
 
 public class SpecificationFuncionario {
@@ -9,5 +11,20 @@ public class SpecificationFuncionario {
     public static Specification<Funcionario> nome(String nome) {
         return ((root, query, criteriaBuilder) ->
             criteriaBuilder.like(root.get("nome"), "%" + nome + "%"));
+    }
+
+    public static Specification<Funcionario> cpf(String cpf) {
+        return (root, query, criteriaBuilder) ->
+                    criteriaBuilder.equal(root.get("cpf"), cpf);
+    }
+
+    public static Specification<Funcionario> salario(Double salario) {
+        return ((root, query, criteriaBuilder) ->
+                    criteriaBuilder.greaterThan(root.get("salario"), salario));
+    }
+
+    public static Specification<Funcionario> dataContratacao(LocalDate dataContratacao) {
+        return ((root, query, criteriaBuilder) ->
+                    criteriaBuilder.greaterThan(root.get("dataContratacao"), dataContratacao));
     }
 }
